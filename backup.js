f4332498a92ba1c5ba02aa7c126bf96f0899198d
beaulazear <beaulazear@gmail.com>
2022-10-06 18:06:04 -0400

    commit tree c984e1874fffcd45b9726adf3786169cf8796a88

---
+++ backup.js
@@ -0,0 +1,4 @@
+function findMatching(array, string) {
+    const matching = array.filter(driver => driver == string);
+    return matching;
+}
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array, string) {
+    let noCap = array.filter(driver => driver.toLowerCase() === string.toLowerCase());
+    return noCap;
+}
+
+function fuzzyMatch(array, string) {
+    let firstFew = array.filter(driver => driver.slice(0,2) === string);
+    return firstFew;
+}
+
+function matchName(array, string) {
+    let matching = array.filter(driver => driver.name === string);
+    return matching;
+}
